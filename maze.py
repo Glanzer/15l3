9317092b6873fdfc6bcbf47503b5f47a273e1210
Glanzer <glanzer.markus@gmail.com>
2016-11-04 17:47:54 +0100

    Algorithm now detects places for doors and passageways. 	modified:   maze.py

--- maze.py
+++ maze.py
@@ -24,8 +24,12 @@ def bimage(M,name,xy,j):
             #print(x,y,M[x][y])
             if M[x][y] is j:
                 im.putpixel((x,y),(250,100,0))
+            elif M[x][y] is 999:
+                im.putpixel((x,y),(250,0,0))
+            elif M[x][y] is 0:
+                im.putpixel((x,y),(75,75,125))
             else:
-                im.putpixel((x,y),(M[x][y]*3,M[x][y]*4,M[x][y]*2))
+                im.putpixel((x,y),(M[x][y]*5,M[x][y]*6,M[x][y]*2))
     #im.show()
     im.save("./"+str(name)+".png","PNG")
 
@@ -73,24 +77,6 @@ def place_rooms(rooms,xy):
 def fix_walls(M,xy):
     for x in range(0,xy[0]):
         for y in range(0,xy[1]):
-            chc=0
-            '''
-            for j in range(-1,2):
-                for k in range(-1,2):
-                    if (x+j<xy[0] and y+k<xy[1]) and (x+j>0 and y+k>0):
-                        if (M[x+j][y+k] is not 0) and (M[x+j][y+k] is not M[x][y]):
-                            chc=chc+1
-            #print(str(chc)+"\t"+str(x)+"/"+str(y))
-            if chc > 1: M[x][y]=0
-            chc=0
-            '''
-            if M[x][y] is not 0:
-                for j in range(-1,2):
-                    for k in range(-1,2):
-                        if (x+j<xy[0] and y+k<xy[1]) and (x+j>0 and y+k>0):
-                            if (M[x+j][y+k] is not M[x][y])and(M[x+j][y+k] is not 0):
-                                chc+=1
-            if chc > 1: M[x][y]=0
             chc=0
             if M[x][y] is not 0:
                 for j in range(-1,2):
@@ -146,17 +132,18 @@ Rooms=get_rooms(Matrix,xy)
 rmlist=[]
 for i in range(0,len(Rooms)):rmlist.append(Rooms[i].name)
 #print(rmlist)
-rmlist.sort()
+#rmlist.sort()
 print(rmlist)
 print("\n")
 
-bimage(Rooms[0].level,"map",xy,Rooms[0].name)
+
 
 #'''
 memo=[]
 for x in range(0,xy[0]):
     for y in range(0,xy[1]):
         count=[0,0]
+        counu=[0,0]
         rooms=[]
         if Rooms[0].level[x][y] is 0:
             for j in range(-1,2):
@@ -165,26 +152,48 @@ for x in range(0,xy[0]):
                         if (Rooms[0].level[x+j][y+k] is 0) and ([x+j,y+k] not in memo): 
                             if j is 0: count[0]=count[0]+1
                             if k is 0: count[1]=count[1]+1
-                        elif (Rooms[0].level[x+j][y+k] is not 0) and (Rooms[0].level[x+j][y+k] not in rooms):
-                            rooms.append(Rooms[0].level[x+j][y+k])
-        #if len(rooms)>1:print(str(rooms)+"\t\t"+str(x)+"/"+str(y))
+                        elif (Rooms[0].level[x+j][y+k] is not 0):
+                            if (Rooms[0].level[x+j][y+k] not in rooms):
+                                rooms.append(Rooms[0].level[x+j][y+k])
+                            if j is -1: counu[0]=counu[0]+1
+                            elif j is 1:counu[0]=counu[0]+1
+                            if k is -1: counu[1]=counu[1]+1
+                            elif k is 1:counu[1]=counu[1]+1
+
+        if len(rooms)is 2:
+            #print(str(rooms)+"\t\t"+str(x)+"/"+str(y))#+"\n"+str(len(rooms)))
             #print(count)
-        #print(count)
-        if (count[0]is 3 or count[1]is 3)and len(rooms)>1:
-            memo.append([rooms,[x,y]])
-#print(memo)
+            #print(count)
+            if len(rooms)>1:
+                if (count[0]> 2 and counu[0]> 4)or(count[1]> 2 and counu[1]> 4):
+                    memo.append([rooms,[x,y]])
+
+for i in range(0,len(memo)):
+    Rooms[0].level[memo[i][1][0]][memo[i][1][1]]=999
+
+bimage(Rooms[0].level,"map",xy,Rooms[0].name)
+
+print(memo)
 #memo[i][0][0]  -> possible Room connecting
 #memo[i][0][1]  -> possible Target-room
 #memo[i][1]     -> possible Doorway coordinates
 #memo[i]        -> possibility #i
+#memo[i]        :: [[RoomA,RoomB],[x,y]]
 
-for i in range(0,len(rmlist)):
+#print(len(Rooms))
+print("\n")
+for i in range(0,len(Rooms)):
+    print(Rooms[i].name)
     for j in range(0,len(memo[0])):
         if Rooms[i].name in memo[j][0]:
             Rooms[i].exits.append(memo[j])
-
-
+    print(Rooms[i].exits)
+'''
 for i in range(0,len(rmlist)):
-    print("Room: "+str(Rooms[i].name)+"\t"+str(Rooms[i].exits))
+    if len(Rooms[i].exits) is 0: print(Rooms[i].name)
+'''
+
+#for i in range(0,len(rmlist)):
+#    print("Room: "+str(Rooms[i].name)+"\t"+str(Rooms[i].exits))
 
 #'''
